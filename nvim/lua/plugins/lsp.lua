214df642b8538c9c4058c3f2b1e5a3d1a9b2f18f
yuusheng <hi@yuusheng.im>
2024-09-03 10:53:45 +0800

    feat: cmdline config remove tab select

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -180,18 +180,18 @@ return {
       local cmp = require("cmp")
       cmp.setup({
         mapping = {
-          ["<Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              local entry = cmp.get_selected_entry()
-              if not entry then
-                cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
-              else
-                cmp.confirm()
-              end
-            else
-              fallback()
-            end
-          end, { "i", "s", "c" }),
+          -- ["<Tab>"] = cmp.mapping(function(fallback)
+          --   if cmp.visible() then
+          --     local entry = cmp.get_selected_entry()
+          --     if not entry then
+          --       cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+          --     else
+          --       cmp.confirm()
+          --     end
+          --   else
+          --     fallback()
+          --   end
+          -- end, { "i", "s", "c" }),
         },
       })
       cmp.setup.cmdline("/", {
