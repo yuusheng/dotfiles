ea6b49c4d3a31e73cedbd959867dd717dcd494d3
yuusheng <hi@yuusheng.im>
2024-09-03 20:25:32 +0800

    chore: update deps

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -36,6 +36,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
     { import = "lazyvim.plugins.extras.editor.mini-files" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
+    { import = "lazyvim.plugins.extras.coding.luasnip" },
     { import = "plugins" },
   },
   defaults = {
