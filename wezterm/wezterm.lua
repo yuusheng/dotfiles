651f038aba8eba83cbbe32a80a33b8f9aea6f06c
yuusheng <hi@yuusheng.im>
2024-09-16 21:33:36 +0800

    feat: add wezterm

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,34 @@
+local wezterm = require("wezterm")
+return {
+	color_scheme = "Catppuccin Mocha",
+	enable_tab_bar = false,
+	font_size = 16.0,
+	font = wezterm.font("JetBrainsMono Nerd Font Mono"),
+	macos_window_background_blur = 30,
+
+	-- window_background_opacity = 0.92,
+	window_background_opacity = 1.0,
+	-- window_background_opacity = 0.78,
+	-- window_background_opacity = 0.20,
+	window_decorations = "RESIZE",
+	keys = {
+		{
+			key = "f",
+			mods = "CTRL",
+			action = wezterm.action.ToggleFullScreen,
+		},
+		{
+			key = "'",
+			mods = "CTRL",
+			action = wezterm.action.ClearScrollback("ScrollbackAndViewport"),
+		},
+	},
+	mouse_bindings = {
+		-- Ctrl-click will open the link under the mouse cursor
+		{
+			event = { Up = { streak = 1, button = "Left" } },
+			mods = "CTRL",
+			action = wezterm.action.OpenLinkAtMouseCursor,
+		},
+	},
+}
