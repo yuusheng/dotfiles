28ad15bd51abe163326d54a09cb8bcfd541e1a90
yuusheng <hi@yuusheng.im>
2024-09-17 18:32:09 +0800

    feat: wezterm config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -3,13 +3,10 @@ return {
 	color_scheme = "Catppuccin Mocha",
 	enable_tab_bar = false,
 	font_size = 16.0,
-	font = wezterm.font("JetBrainsMono Nerd Font Mono"),
+	font = wezterm.font("JetBrainsMono Nerd Font Mono", { weight = "Bold" }),
 	macos_window_background_blur = 30,
 
-	-- window_background_opacity = 0.92,
-	window_background_opacity = 1.0,
-	-- window_background_opacity = 0.78,
-	-- window_background_opacity = 0.20,
+	window_background_opacity = 0.92,
 	window_decorations = "RESIZE",
 	keys = {
 		{
@@ -18,8 +15,8 @@ return {
 			action = wezterm.action.ToggleFullScreen,
 		},
 		{
-			key = "'",
-			mods = "CTRL",
+			key = "k",
+			mods = "SUPER",
 			action = wezterm.action.ClearScrollback("ScrollbackAndViewport"),
 		},
 	},
