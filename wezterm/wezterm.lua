ef580f1d977a3680a91134c4d81f2f06846f2fd6
yuusheng <hi@yuusheng.im>
2024-09-17 22:33:52 +0800

    feat: wezterm tab

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,7 +1,8 @@
 local wezterm = require("wezterm")
-return {
+
+local config = {
 	color_scheme = "Catppuccin Mocha",
-	enable_tab_bar = false,
+	-- enable_tab_bar = false,
 	font_size = 16.0,
 	font = wezterm.font("JetBrainsMono Nerd Font Mono", { weight = "Bold" }),
 	macos_window_background_blur = 30,
@@ -29,3 +30,38 @@ return {
 		},
 	},
 }
+
+wezterm.plugin.require("https://github.com/nekowinston/wezterm-bar").apply_to_config(config, {
+	position = "top",
+	max_width = 32,
+	dividers = "slant_right", -- or "slant_left", "arrows", "rounded", false
+	indicator = {
+		leader = {
+			enabled = true,
+			off = " ",
+			on = " ",
+		},
+		mode = {
+			enabled = true,
+			names = {
+				resize_mode = "RESIZE",
+				copy_mode = "VISUAL",
+				search_mode = "SEARCH",
+			},
+		},
+	},
+	tabs = {
+		numerals = "arabic", -- or "roman"
+		pane_count = "superscript", -- or "subscript", false
+		brackets = {
+			active = { "", ":" },
+			inactive = { "", ":" },
+		},
+	},
+	clock = { -- note that this overrides the whole set_right_status
+		enabled = true,
+		format = "%H:%M", -- use https://wezfurlong.org/wezterm/config/lua/wezterm.time/Time/format.html
+	},
+})
+
+return config
