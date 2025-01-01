9624e53b65f05b8f00adf10f46e8d9f8ef1e88c6
yuusheng <hi@yuusheng.im>
2025-01-01 17:42:44 +0800

    feat: add yazi plugin

--- zsh/init.sh
+++ zsh/init.sh
@@ -1,3 +1,6 @@
+# download Homebrew 
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
 # download oh my zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
