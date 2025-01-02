ff1eaa09774fbc9f7a49ee87e2527a4765c12628
yuusheng <hi@yuusheng.im>
2025-01-02 23:25:11 +0800

    feat: add more avante config

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -14,6 +14,25 @@ return {
     },
   },
   {
+    "blink.cmp",
+    opts = {
+      keymap = {
+        ["<Tab>"] = {
+          function(cmp)
+            if cmp.snippet_active() then
+              return cmp.accept()
+            else
+              return cmp.select_and_accept()
+            end
+          end,
+          "snippet_forward",
+          "fallback",
+        },
+        ["<S-Tab>"] = { "snippet_backward", "fallback" },
+      },
+    },
+  },
+  {
     "echasnovski/mini.files",
     event = "VeryLazy",
     opts = {
