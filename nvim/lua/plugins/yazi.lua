02bceee19c6021dc2516cb6e03aba8c42916a470
yuusheng <yuusheng7@gmail.com>
2025-01-07 23:15:03 +0800

    feat: yazi config

--- nvim/lua/plugins/yazi.lua
+++ nvim/lua/plugins/yazi.lua
@@ -18,7 +18,7 @@ return {
     {
       -- NOTE: this requires a version of yazi that includes
       -- https://github.com/sxyazi/yazi/pull/1305 from 2024-07-18
-      "<C-p>",
+      "<C-j>",
       "<cmd>Yazi toggle<cr>",
       desc = "Resume the last yazi session",
     },
