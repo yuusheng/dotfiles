8437c854b9315b54f3a4c32fdd2bf744428b0a3a
yuusheng <hi@yuusheng.im>
2025-05-24 11:24:43 +0800

    feat: add rustaceanvim for now

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -55,6 +55,7 @@ require("lazy").setup({
       },
     },
     { import = "plugins" },
+    { import = "plugins/lang" },
   },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
