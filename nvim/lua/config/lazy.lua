7eac92a752e3557e12dead7160f31084ba447805
yuusheng <yuusheng7@gmail.com>
2025-10-26 13:39:01 +0800

    feat: update plugins

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -17,60 +17,19 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
-    {
-      "LazyVim/LazyVim",
-      import = "lazyvim.plugins",
-      opts = {
-        colorscheme = "catppuccin-mocha",
-        news = {
-          lazyvim = true,
-          neovim = true,
-        },
-        kind_filter = {
-          javascript = {
-            "Class",
-            "Constructor",
-            "Enum",
-            "Function",
-            "Interface",
-            "Module",
-            "Method",
-            "Struct",
-            "Variable",
-            "Type",
-          },
-          typescript = {
-            "Class",
-            "Constructor",
-            "Enum",
-            "Function",
-            "Interface",
-            "Module",
-            "Method",
-            "Struct",
-            "Variable",
-            "Type",
-          },
-        },
-      },
-    },
+    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     { import = "plugins" },
     { import = "plugins/lang" },
   },
   defaults = {
-    -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
-    -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
     lazy = false,
-    -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
-    -- have outdated releases, which may break your Neovim install.
-    version = false, -- always use the latest git commit
-    -- version = "*", -- try installing the latest stable version for plugins that support semver
+    version = false,
   },
   install = { colorscheme = { "catppuccin-mocha" } },
   checker = {
-    enabled = true, -- check for plugin updates periodically
-    notify = false, -- notify on update
-  }, -- automatically check for plugin updates
+    enabled = true,
+    notify = false,
+  },
   performance = {
     rtp = {
       -- disable some rtp plugins
--- nvim/lua/plugins/core.lua
+++ nvim/lua/plugins/core.lua
@@ -1,4 +1,40 @@
 return {
   { "folke/lazy.nvim", version = false },
-  { "LazyVim/LazyVim", version = false },
+  {
+    "LazyVim/LazyVim",
+    version = false,
+    opts = {
+      colorscheme = "catppuccin-mocha",
+      news = {
+        lazyvim = true,
+        neovim = true,
+      },
+      kind_filter = {
+        javascript = {
+          "Class",
+          "Constructor",
+          "Enum",
+          "Function",
+          "Interface",
+          "Module",
+          "Method",
+          "Struct",
+          "Variable",
+          "Type",
+        },
+        typescript = {
+          "Class",
+          "Constructor",
+          "Enum",
+          "Function",
+          "Interface",
+          "Module",
+          "Method",
+          "Struct",
+          "Variable",
+          "Type",
+        },
+      },
+    },
+  },
 }
