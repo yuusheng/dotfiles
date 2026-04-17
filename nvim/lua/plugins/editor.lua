0f26b575567d9d771933fcc82dd3a862354f5a81
yuusheng <yuusheng7@gmail.com>
2026-04-17 19:09:44 +0800

    feat: leetcode nvim

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -370,4 +370,52 @@ return {
       },
     },
   },
+  {
+    "kawre/leetcode.nvim",
+    opts = {
+      lang = "cpp",
+      cn = {
+        enabled = true, ---@type boolean
+        translator = true, ---@type boolean
+        translate_problems = true, ---@type boolean
+      },
+      injector = {
+        ["rust"] = {
+          before = { "#[allow(dead_code)]", "fn main(){}", "#[allow(dead_code)]", "struct Solution;" },
+        },
+      },
+      hooks = {
+        ["question_enter"] = {
+          function(question)
+            if question.lang ~= "rust" then
+              return
+            end
+            local problem_dir = vim.fn.stdpath("data") .. "/leetcode/Cargo.toml"
+            local content = [[
+              [package]
+              name = "leetcode"
+              edition = "2024"
+
+              [lib]
+              name = "%s"
+              path = "%s"
+
+              [dependencies]
+              rand = "0.10"
+              regex = "1"
+              itertools = "0.14.0"
+            ]]
+            local file = io.open(problem_dir, "w")
+            if file then
+              local formatted = (content:gsub(" +", "")):format(question.q.frontend_id, question:path())
+              file:write(formatted)
+              file:close()
+            else
+              print("Failed to open file: " .. problem_dir)
+            end
+          end,
+        },
+      },
+    },
+  },
 }
